a736f0d2bf66d00315aca3f1b84831feed190d00
Emily Chu <Emily.H.Chu@gmail.com>
2015-04-07 23:46:44 -0400

    need to fix up budget table display

--- app/controllers/budget_controller.rb
+++ app/controllers/budget_controller.rb
@@ -1,6 +1,32 @@
 class BudgetController < ApplicationController
   
   def index
-    @budget=Category.all
+    @budget=Budget.all
   end
+
+  def create
+    @budget = Budget.new(budget_params)
+    if @budget.save
+      redirect_to budget_index_path
+    else
+      flash[:error] = "Something went wrong!"
+    end
+  end
+
+  def update
+    @budget = Budget.find(params[:id])
+    if @budget.update(budget_params)
+      redirect_to budget_index_path(@budget)
+    else
+      flash[:error] = "Something went wrong!"
+      render :index 
+    end
+  end
+
+  private
+    def budget_params
+      # params.require(:budget).permit(:category, :cost)
+      params.permit("category", "cost")
+    end
+
 end
